b399b88636c72c09d79438466dcff2f200f2dcb4
Thiago da Silva Machado <thiagodasilvamachadoo44@gmail.com>
2023-03-31 20:38:44 -0300

    Initial commit

---
+++ src/test/java/io/github/thiagomachadoo/microservicecloudgateway/MicroserviceCloudGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.thiagomachadoo.microservicecloudgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceCloudGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
